d7224fb7bdf964c0c42a7ada35cec209d9d07563
MateusKha <mateusgnascimento@outlook.com>
2021-08-27 20:27:44 -0300

    Finish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,40 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useDebugValue, useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldTasks) => [...oldTasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks((oldTasks) => {
+      let newTasks = [...oldTasks];
+      newTasks.forEach((value) => {
+        if (value.id === id) value.done = !value.done;
+      });
+      return newTasks;
+    });
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    console.log(tasks);
+    setTasks((oldTasks) => {
+      let newTasks = [...oldTasks];
+      newTasks = newTasks.filter((value) => value.id !== id);
+      return newTasks;
+    });
   }
 
   return (
@@ -26,18 +43,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
